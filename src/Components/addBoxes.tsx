e53556b9dd374534e005b68390833c84dd647acb
Andreas Öster <andost@dtv.local>
2024-10-16 18:52:36 +0200

    Second commit

--- src/Components/addBoxes.tsx
+++ src/Components/addBoxes.tsx
@@ -3,23 +3,12 @@ import newsList from "./temp";
 function AddBoxes() {
   let content = document.querySelector(".content");
 
-  const ColorList: string[] = [
-    "#FF5733", // Bright Red-Orange
-    "#33FF57", // Bright Green
-    "#3357FF", // Bright Blue
-    "#FF33A1", // Bright Pink
-    "#F7FF33", // Bright Yellow
-    "#33FFF7", // Bright Cyan
-    "#FF3333", // Bright Red
-    "#FF8333", // Bright Orange
-    "#33FF33", // Bright Lime Green
-  ];
+  const ColorList = ["#f1e8dd", "#f9ccb7", "#d4e0e5"];
 
   const getRandomColor = () => {
     let randomNumber = Math.floor(Math.random() * ColorList.length);
-    let SelectedColor = parseInt(ColorList[randomNumber], 10);
-    console.log(randomNumber);
-    return SelectedColor;
+    // let SelectedColor = parseInt(ColorList[randomNumber], 10);
+    return ColorList[randomNumber];
   };
   getRandomColor();
   return (
@@ -28,7 +17,7 @@ function AddBoxes() {
         <div
           key={index}
           className="news-item"
-          style={{ backgroundColor: getRandomColor }}
+          style={{ backgroundColor: getRandomColor() }}
         >
           <h3>{item.NewsTitle}</h3>
           <p>
