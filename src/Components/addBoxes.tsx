a0832cda537768a3f9134f24141e58860bda5a30
Andreas Öster <andost@dtv.local>
2024-10-17 14:41:54 +0200

    Third commit

--- src/Components/addBoxes.tsx
+++ src/Components/addBoxes.tsx
@@ -1,13 +1,10 @@
 import newsList from "./temp";
 
 function AddBoxes() {
-  let content = document.querySelector(".content");
-
   const ColorList = ["#f1e8dd", "#f9ccb7", "#d4e0e5"];
 
   const getRandomColor = () => {
     let randomNumber = Math.floor(Math.random() * ColorList.length);
-    // let SelectedColor = parseInt(ColorList[randomNumber], 10);
     return ColorList[randomNumber];
   };
   getRandomColor();
